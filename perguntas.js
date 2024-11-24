b90baa64967419d425f0d2e3afe474a994e66c5d
VTMNC feber <168595872+Odeio0Feder@users.noreply.github.com>
2024-11-24 18:21:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    '1º Negação',
+    '*Conhece a piada do pônei?',
+    '-Pô nei eu...'
+)
+
+criaCartao(
+    '2º Raiva',
+    '*Como o pato foi pra Macapá?',
+    'Sei não irmão ta me achando com cara de mapa f#@$%!!! '
+)
+
+criaCartao(
+    '3º Barganha',
+    'Vire a carta...',
+    'Vire a carta...'
+)
+
+criaCartao(
+    '4º Depressão',
+    'Nasci cedo demais para ver carros voadores',
+    'E tarde demais para comprar terreno barato... :('
+)
+
+criaCartao(
+    '5º Aceitação',
+    'Por que a galinha bateu a cabeça na parede?',
+    'Para ficar com galo HAHAHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAH'
+)
